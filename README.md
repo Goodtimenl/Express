d31ef136c5f4063adcf70f18f4d4d7238f7c3494
Goodtimenl <gregory.delaloeuf@gmail.com>
2024-02-07 12:33:37 +0100

    add readme preveiw code

---
+++ README.md
@@ -0,0 +1,4 @@
+# Express
+
+Preview du code avec les 4 tests qui ont fonctionnés
+<a href="https://ibb.co/L597Ly4"><img src="https://i.ibb.co/KwVvMgC/Capture-d-e-cran-2024-02-06-a-15-27-00.png" alt="Capture-d-e-cran-2024-02-06-a-15-27-00" border="0"></a>
--- test/movies.test.js
+++ test/movies.test.js
@@ -15,12 +15,12 @@ describe("GET /api/movies/:id", () => {
   });
 
   test("pour l'id 1, retourne une réponse JSON", async () => {
-    const response = await request(app).get("/api/movies/1");
-    expect(response.headers["content-type"]).toMatch(/json/);
+    const response2 = await request(app).get("/api/movies/1");
+    expect(response2.headers["content-type"]).toMatch(/json/);
   });
 
-  test("pour l'id 0, retourne statut 404 (Not Found)", async () => {
-    const response = await request(app).get("/api/movies/0");
-    expect(response.status).toBe(404);
+  test("pour l'id 0, retourne un statut 404", async () => {
+    const response3 = await request(app).get("/api/movies/0");
+    expect(response3.status).toBe(404);
   });
 });
